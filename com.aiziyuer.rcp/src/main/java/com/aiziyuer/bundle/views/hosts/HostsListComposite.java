658940c586794799cffa1ccec76bcf62f07929f9
aiziyuer <ziyu0123456789@gmail.com>
2017-04-15 00:53:20 +0800

    新增一个viewpart来实现hosts的信息

---
+++ com.aiziyuer.rcp/src/main/java/com/aiziyuer/bundle/views/hosts/HostsListComposite.java
@@ -0,0 +1,17 @@
+package com.aiziyuer.bundle.views.hosts;
+
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+
+import com.aiziyuer.bundle.framework.common.ui.AbstractComposite;
+
+public class HostsListComposite extends AbstractComposite {
+
+	public HostsListComposite(Composite parent, int style) {
+		super(parent, style);
+		setLayout(new FillLayout());
+	}
+	
+	
+
+}
